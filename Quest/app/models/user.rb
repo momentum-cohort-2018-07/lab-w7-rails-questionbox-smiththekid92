a3f51b37a639ae1e2894dbe35a47fca03febf70f
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-09-10 14:58:02 -0400

    Added seed data for users. Updated gemfile for required user gems

--- Quest/app/models/user.rb
+++ Quest/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, :email, presence: true, uninqueness: true
+  validates :username, :email, presence: true, uniqueness: true
   validates :password, presence: true, length:{minimum:8}
 
   # has_many :questions
--- Quest/db/seeds.rb
+++ Quest/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+300.times do
+  User.create(
+    username: Faker::Internet.username,
+    email: Faker::Internet.free_email, 
+    password: Faker::Internet.password(8)
+  )
+end
