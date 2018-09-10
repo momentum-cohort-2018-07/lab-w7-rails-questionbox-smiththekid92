250f1bb6df27455fb017f9b60c57287cea3c87aa
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-09-10 17:41:47 -0400

    Readded questions model & controller

---
+++ Quest/app/controllers/questions_controller.rb
@@ -0,0 +1,53 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+      @question = Question.find(params[:id])
+  end
+
+  def new
+    redirect_to new_session_path, notice: 'You must be logged in to add a question' if !current_user
+    @question = Question.new
+  end
+
+  def edit
+      @question = Question.find(params[:id])
+  end
+
+  def create
+      @question = Question.new(question_params)
+
+      if @question.save
+          redirect_to @question
+      else
+          render 'new'
+      end
+  end
+
+  def update
+      @question = Question.find(params[:id])
+
+      if @question.update(question_params)
+
+          redirect_to @question
+      else
+          render 'edit'
+      end
+  end
+
+  def destroy
+      @question = Question.find(params[:id])
+      @question.destroy
+
+      redirect_to questions_path
+  end
+
+
+  private
+  def question_params
+      params.require(:question).permit(:title, :body, :username, :user_id)
+  end
+
+end
